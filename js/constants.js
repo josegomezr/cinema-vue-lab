f41e45a481070edb72b8d93e1048d2e975f9d9f4
José D. Gómez R <1josegomezr@gmail.com>
2018-12-25 21:37:08 -0400

    fixing mixed content

--- js/constants.js
+++ js/constants.js
@@ -1,6 +1,6 @@
 (function(window, Vue, $){
-  var BASE = "http://movie-ves2.ddns.net";
-  var API_KEY = 'rjnnH~tc1UAiSKf3Fe7x=Vyf4y=0Senb09dy=IDTFPw-Dvqrp76%WDPJcSPW';
+  var BASE = "da-domain";
+  var API_KEY = 'da-key';
   window.CARTELERA_URL = function(id){
     // return 'json/json-cartelera.json';
     return BASE + '/cartelera';
